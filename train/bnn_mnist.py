d774ad134d3873bda499582c9f5445afaba74624
czgdp1807 <singh.23@iitj.ac.in>
2020-03-05 18:52:43 +0530

    add save weights

--- train/bnn_mnist.py
+++ train/bnn_mnist.py
@@ -12,7 +12,7 @@ def take_subset(x, y, size, count):
     return x[count*size:count*size + size], \
            y[count*size:count*size + size]
 
-def train(input_data_path, target_data_path, batch_size=1, epochs=1,
+def train(input_data_path, target_data_path, weight_file, batch_size=1, epochs=1,
           cache_result=True, use_cache=False):
     inputs = read_idx(input_data_path, True, True, True, 10000, False, cache_result, use_cache)
     targets = read_idx(target_data_path, False, False, True, 10000, True, cache_result, use_cache)
@@ -28,3 +28,4 @@ def train(input_data_path, target_data_path, batch_size=1, epochs=1,
             input_sub, target_sub = take_subset(inputs, targets, batch_size, batch)
             model.learn(input_sub, target_sub, 1/(size//batch_size))
         print("Loss at completion of epoch %s is %s"%(epoch, model.get_loss(inputs, targets, 10)))
+    model.save_weights("./weights/" + weight_file)
--- train_mnist_script.py
+++ train_mnist_script.py
@@ -15,6 +15,8 @@ cache_result: bool
       to save processing time and computation power.
 use_cache: bool
       Set 1 is cached pre-processed data is to be used.
+weight_file: str
+      The file where weights are to be saved.
 """
 from train.bnn_mnist import train
 import sys
@@ -24,7 +26,8 @@ batch_size = int(sys.argv[3])
 epochs = int(sys.argv[4])
 cache_result = bool(int(sys.argv[5]))
 use_cache = bool(int(sys.argv[6]))
+weight_file = sys.argv[7]
 
 
-train(input_data_path, output_data_path,
+train(input_data_path, output_data_path, weight_file,
       batch_size, epochs, cache_result, use_cache)
