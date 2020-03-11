0a75a9fb84c7e13e3e1a2371db4981f7f2ba7926
czgdp1807 <singh.23@iitj.ac.in>
2020-03-11 19:58:46 +0530

    modified a bit

--- train/bnn_mnist.py
+++ train/bnn_mnist.py
@@ -25,7 +25,10 @@ def train(input_data_path, target_data_path, batch_size=1, dataset_size=1,
     logs.write("Number of batches per epoch: " + str(size//batch_size) + '\n')
     logs.write("Number of epochs: " + str(epochs) + '\n')
     logs.write("Initial Loss %s"%(model.get_loss(inputs, targets, 10)) + '\n')
+    logs.close()
     for epoch in range(epochs):
+        if epoch%20 == 0:
+            logs = open("./logs/"+curr_date_time, 'a')
         inputs, targets = random_shuffle(inputs, targets)
         for batch in range(size//batch_size):
             input_sub, target_sub = take_subset(inputs, targets, batch_size, batch)
@@ -34,3 +37,4 @@ def train(input_data_path, target_data_path, batch_size=1, dataset_size=1,
         logs.write("Cross entropy at completion of epoch %s is %s"%(epoch,model.get_loss(inputs, targets, 1, 1., True)) + '\n')
         if epoch%20 == 0:
             model.save_weights("./weights/mnist/" + curr_date_time)
+            logs.close()
